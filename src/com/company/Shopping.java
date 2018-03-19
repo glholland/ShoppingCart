d86f1d48beea6d5894499f150917ac0f4a5cb480
Garrett Holland <igotsol@live.com>
2018-03-18 20:06:52 -0400

    Commented Half of the code.

--- src/com/company/Shopping.java
+++ src/com/company/Shopping.java
@@ -9,21 +9,24 @@ public class Shopping {
         int choice = 1, quantity;
         String name;
         double price;
-
+        //Create Objects to be used for the Shopping program. DecimalFormat for money formatting. ShoppingCart contains the array of Items. Scanner scans.
         DecimalFormat df = new DecimalFormat("#.00");
         ShoppingCart cart = new ShoppingCart(5);
         Scanner scan = new Scanner(System.in);
 
-
+        //Welcome
         System.out.println("Welcome to the Virtual Shopping Cart");
         System.out.println("Please choose from one of the following options!");
-
+        //When user chooses to quit, let program run to end.
         while (choice != 2) {
-
+            //Display options
             System.out.println("\nOptions\n---------------------------------\n1. Add item to cart.\n2. Quit.");
+            //Scan for user choice
             choice = scan.nextInt();
+            //Could have used an if statement here.
             switch(choice){
             case 1:
+                //Gather user input about the current Item to be created and added to the cart.
                 System.out.print("Name of item to enter: ");
                 name = scan.next();
                 System.out.println("");
@@ -32,6 +35,7 @@ public class Shopping {
                 System.out.println("");
                 System.out.println("Price of item: ");
                 price = scan.nextDouble();
+                //Passes the attributed to addToCart - this will create the Item and place it in the array.
                 cart.addToCart(name, price, quantity);
                 System.out.print("");
                 //Print the Cart to display
@@ -46,6 +50,7 @@ public class Shopping {
             }
 
         }
+        //Thank the user
         System.out.println("Thanks for shopping today!!! Make sure to spend more next time!");
         System.out.println("Please pay $" + df.format(cart.getCartTotal()) + ".");
     }
--- src/com/company/ShoppingCart.java
+++ src/com/company/ShoppingCart.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class ShoppingCart {
+    //Initialize variables to be used.
     int itemCount, capacity;
     double totalPrice = 0.0;
     Item[] cart;
@@ -13,25 +14,33 @@ public class ShoppingCart {
         totalPrice = 0;
     }
     public void addToCart(String name, double price, int quantity){
+        //Passes args to another constructor, creates the Item.
         Item item = new Item(name, price, quantity);
+        //Checks to see if the itemCount is at the correct spot for the size of the array.
         if(itemCount < cart.length) {
+            //Adds the newly created item to the array
             cart[itemCount] = item;
+            //Increment the itemCount
             itemCount++;
         }else{
+            //If the index of the array is larger or equal to the length
             increaseSize();
+            //Then add the item to the new bigger array.
             cart[itemCount] = item;
+            //Increment
             itemCount++;
         }
     }
 
     public void increaseSize(){
+        //Creates a new Item array to the length of the current size + 3
         Item[] temp = new Item[cart.length + 3];
-
         System.out.println("Increased cart size.");
+        //Copies old array to new temp array.
         for (int i = 0; i < cart.length; i++) {
             temp[i] = cart[i];
         }
-
+        //Points the name of the old array to the new temp array so we can access it using the same name.
         cart = temp;
     }
 
